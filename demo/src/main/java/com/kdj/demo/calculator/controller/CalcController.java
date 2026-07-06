5cfc6c739088982f3d31715037c68781207634a1
Kalas134 <dk6249@gmail.com>
2026-07-06 17:27:47 +0900

    Calculator commit
    Calculator commit

--- demo/src/main/java/com/kdj/demo/calculator/controller/CalcController.java
+++ demo/src/main/java/com/kdj/demo/calculator/controller/CalcController.java
@@ -3,20 +3,61 @@ package com.kdj.demo.calculator.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kdj.demo.calculator.service.CalcService;
 
+@RestController
 public class CalcController {
 	
-	@Autowired
-	private CalcService calcService;
+////A.자바방식의 개발자가 직접 Obj를 관리는 방식
+//	private CalcService calcService = new CalcService();
+//	
+//	@GetMapping("/add")
+//	public String add(@RequestParam int a, @RequestParam int b) {
+//		// 서비스단에서 구현해야할 계산 알고리즘.
+//		int result = a + b;
+//		return "비즈니스 로직을 직접 구현해서 값을 주는 방식: " + result ;
+//	}
 	
-//	public CalcController(CalcService calcService) {
+//	B. 스프링 방식: DI1: @Autowired 방식
+//	@Autowired
+//	private CalcService calcService;
+	
+//	----------------
+//	C. 스프링 방식: getMethod를 이용한 서비스단 사용방식
+//	private CalcService calcService;
+//	
+//	public void getCalc(CalcService calcService) {
 //		this.calcService = calcService;
 //	}
 	
-	@GetMapping("/add")
-	public int add (@RequestParam int a, @RequestParam int b) {
-		return calcService.add(a, b);
+//	D. 스프링 방식: 생성자를 이용한 서비스단 사용방식
+	
+	private final CalcService calcService;
+	
+	public CalcController(CalcService calcService) {
+		this.calcService = calcService;
+	}
+	
+	// http://localhost:8081/add-calc?a=10&b=20
+	@GetMapping("/add-calc")
+	public int addCalc(@RequestParam int a, @RequestParam int b) {
+		return calcService.addCalc(a, b);
+	}
+	
+	@GetMapping("/subtract-calc")
+	public int subtractCalc (@RequestParam int a, @RequestParam int b) {
+		return calcService.subtractCalc(a, b);
+	}
+	
+	@GetMapping("/multiply-calc")
+	public int multiplyCalc (@RequestParam int a, @RequestParam int b) {
+		return calcService.multiplyCalc(a, b);
+	}
+	
+	@GetMapping("/divide-calc")
+	public int divideCalc (@RequestParam int a, @RequestParam int b) {
+		return calcService.divideCalc(a, b);
 	}
 }
--- demo/src/main/java/com/kdj/demo/calculator/service/CalcService.java
+++ demo/src/main/java/com/kdj/demo/calculator/service/CalcService.java
@@ -1,9 +1,27 @@
 package com.kdj.demo.calculator.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalcService {
 
-	public int add(int a, int b) {
+	public int addCalc(int a, int b) {
 		return a + b;
+	}
+
+	public int subtractCalc(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplyCalc(int a, int b) {
+		return a * b;
+	}
+
+	public int divideCalc(int a, int b) {
+		if (b == 0) {
+			throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+		}
+		return a / b;
 	}
 
 }
